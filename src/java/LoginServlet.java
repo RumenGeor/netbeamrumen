168adac7d6eda8f7315e6b62219869f8f0f4967a
zebra <zebra@gmail.com>
2023-03-31 11:44:45 +0300

    netbean

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       PrintWriter writer = response.getWriter();
+       String user = request.getParameter("user");
+       String pass = request.getParameter("pass");
+       if (user.equals("Georgiev") && pass.equals("1255"))
+       writer.println("Successful login");
+       
+    } else {
+    writer.println("Failed");
+}
+}
